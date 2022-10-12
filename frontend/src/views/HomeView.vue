71b6d91254534fa57dc519e58f256fb5ac348c7d
Christian Cuevas <ccpotential@gmail.com>
2022-10-12 01:49:05 -0700

    Draft Navbar

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,26 @@
 <template>
   <div class="home">
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="img/icons/favicon.svg"
+            alt=""
+            width="32"
+            height="32"
+            class="d-inline-block align-text-top"
+          />
+          <span class="text-success">Open</span>
+          <span class="text-danger">Heart</span>
+        </a>
+
+        <form class="d-flex">
+          <select class="form-select" aria-label="Default select example">
+            <option>English</option>
+            <option>Español</option>
+          </select>
+        </form>
+      </div>
+    </nav>
   </div>
 </template>
