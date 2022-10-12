dc0316e10345e561f391d78088e99afba5d44ad0
Christian Cuevas <ccpotential@gmail.com>
2022-10-12 02:06:33 -0700

    Draft Home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <nav class="navbar navbar-light bg-light">
+  <div class="home h-100">
+    <nav class="navbar navbar-light bg-light fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img
@@ -22,5 +22,33 @@
         </form>
       </div>
     </nav>
+
+    <div class="h-100 p-5 d-flex flex-column">
+      <p class="mt-3 p-4 flex-fill text-center">
+        A Progressive Web Application enabling individuals to compare cost of medication
+        and medical services; while analyzing trends to improve decision making for the
+        consumer and provider.
+      </p>
+
+      <ul class="m-3 nav nav-pills justify-content-center">
+        <li class="px-1 nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Medicine</a>
+        </li>
+        <li class="px-1 nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+      </ul>
+
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="Search"
+        />
+        <button class="btn btn-outline-success" type="button" id="Search">Compare</button>
+      </div>
+    </div>
   </div>
 </template>
