92cec42c0aca598ef03bb79efcee2b6aabe2b099
Christian Cuevas <ccpotential@gmail.com>
2022-10-12 01:32:31 -0700

    Expand app to fit screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+
 @import'~bootstrap/dist/css/bootstrap.css'
 </style>
