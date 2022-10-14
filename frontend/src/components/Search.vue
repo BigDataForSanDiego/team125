4645de862f79231b34044d36f1b1653d14f11cd1
Christian Cuevas <ccpotential@gmail.com>
2022-10-13 19:52:54 -0700

    Internationalization of Search

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -2,14 +2,14 @@
   <input
     type="text"
     class="form-control"
-    placeholder="Search"
-    aria-label="Search"
-    aria-describedby="Search"
+    :placeholder="_i18n('search')"
     @input="_onSearchInput($event.target.value)"
   />
 </template>
 
 <script>
+import Session from "@/core/session";
+
 export default {
   name: "Search",
   props: {
@@ -24,12 +24,26 @@ export default {
     return {
       _searching: false,
       search: "",
+
+      _session: Session,
     };
   },
   methods: {
     _onSearchInput: function (search) {
       console.log(`searching ${this.search_type} for '${search}'...`);
       this.search = search;
+    },
+
+    _i18n: function (i18n_id) {
+      const internationalization = {
+        english: {
+          search: "Search",
+        },
+        spanish: {
+          search: "Buscar",
+        },
+      };
+      return internationalization[this._session.language][i18n_id];
     },
   },
 };
