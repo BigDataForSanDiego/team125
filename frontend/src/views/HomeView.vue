5fabebd4c0af3faa59938552f42a366248f34c4f
Christian Cuevas <ccpotential@gmail.com>
2022-10-13 18:56:19 -0700

    Add Particle.js

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,12 +2,18 @@
   <div class="home h-100">
     <Navbar />
 
+    <div
+      class="w-100 h-100 position-absolute"
+      style="z-index: -1"
+      id="particles-js"
+    ></div>
+
     <div class="h-100 p-5 d-flex flex-column">
-      <p class="description mt-3 p-4 flex-fill text-center">
+      <i class="description mt-3 p-4 flex-fill text-center">
         A Progressive Web Application enabling individuals to compare cost of medication
         and medical services; while analyzing trends to improve decision making for the
         consumer and provider.
-      </p>
+      </i>
 
       <ul class="m-3 nav nav-pills justify-content-center">
         <li class="px-1 nav-item">
@@ -63,6 +69,8 @@
 import Navbar from "@/components/Navbar.vue";
 import Search from "@/components/Search.vue";
 
+import "particles.js";
+
 export default {
   name: "HomeView",
   components: {
@@ -73,6 +81,66 @@ export default {
     return {
       _looking_for: "medicine",
     };
+  },
+  mounted: function () {
+    particlesJS("particles-js", {
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: ["#00ff00", "ff0000"],
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#00ff00",
+          },
+          polygon: {
+            nb_sides: 5,
+          },
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 10,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false,
+          },
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      retina_detect: true,
+    });
   },
   methods: {
     _setLookingFor: function (looking_for) {
@@ -94,6 +162,10 @@ export default {
 <style>
 .nav-link {
   cursor: pointer;
+}
+
+* {
+  text-shadow: 0px 0px 1px;
 }
 
 @media (min-width: 576px) {
